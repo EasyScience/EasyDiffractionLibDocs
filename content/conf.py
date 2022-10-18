83f687db389b11f570b434950309107ae7d1c317
Andrew Sazonov <andrew.sazonov@esss.eu>
2022-10-18 20:03:07 +0200

    Disable Jekyll build (fix underscores in names)

--- content/conf.py
+++ content/conf.py
@@ -14,25 +14,22 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-#extensions = ['myst_parser', 'myst_nb']
 extensions = ['myst_nb']
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-myst_enable_extensions = ['amsmath',
-                          'dollarmath']
+myst_enable_extensions = ['amsmath', 'dollarmath']
 
-nb_execution_mode = 'force'  # Fore run all notebooks not included in nb_execution_excludepatterns
+nb_execution_mode = 'force'  # Force run all notebooks not included in nb_execution_excludepatterns
 nb_execution_excludepatterns = ['advanced/*.ipynb']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_book_theme"
 html_static_path = ['_static']
-
+html_extra_path = ['_static/.nojekyll']
 html_logo = "_static/logo_wname.png"
-html_theme_options = {
-    "logo_only": True,
-}
+
+html_theme = "sphinx_book_theme"
+html_theme_options = { "logo_only": True }
